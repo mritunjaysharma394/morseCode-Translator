075577e0bf1c79e401ff5528eae65de7613fe548
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-12-07 16:10:56 +0530

    css styling and html

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputTxt = document.querySelector("#inputTxt");
+var outputTxt = document.querySelector("#outputTxt");
+
+var serverURL = "https://api.funtranslations.com/translate/morse.json"
+
